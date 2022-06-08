5d5427a81e882bab78f3518d23e05329ec84f2d9
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-06-08 23:32:21 +0900

    Initial Commit

---
+++ plic/plicApp.swift
@@ -0,0 +1,20 @@
+//
+//  plicApp.swift
+//  plic
+//
+//  Created by 이주화 on 2022/06/08.
+//
+
+import SwiftUI
+
+@main
+struct plicApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
