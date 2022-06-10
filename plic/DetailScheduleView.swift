059356a67743324ef313a48ef8e2be99d9828de0
Youngseo Yoon <58525719+yys88699@users.noreply.github.com>
2022-06-10 21:24:54 +0900

    💄 네비게이션 바 생성
    - 상세 정보 뷰 네비게이션 바 생성
    - 임의로 ContentView로 이동하게 설정

---
+++ plic/DetailScheduleView.swift
@@ -0,0 +1,33 @@
+//
+//  DetailScheduleView.swift
+//  plic
+//
+//  Created by Youngseo Yoon on 2022/06/10.
+//
+
+import SwiftUI
+
+struct DetailScheduleView: View {
+    var body: some View {
+        NavigationView{
+            Text("뷰 들어갈 곳")
+            //네비게이션 설정 (임의로 ContentView)
+                .navigationBarTitle("세부 일정", displayMode: .inline)
+                .navigationBarItems(
+                    leading: NavigationLink(destination: ContentView(), label: {
+                        Image(systemName: "chevron.left")
+                        Text("오늘의 일정")
+                    }),
+                    trailing: NavigationLink(destination: ContentView(), label: {
+                        Text("Edit")
+                    })
+                )
+        }
+    }
+}
+
+struct DetailScheduleView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailScheduleView()
+    }
+}
