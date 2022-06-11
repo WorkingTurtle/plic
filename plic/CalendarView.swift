d9102740b68533f7e6a501391d4d4ed7a1b21de3
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-06-11 22:05:48 +0900

    :lipstick: Update CalendarView

--- plic/CalendarView.swift
+++ plic/CalendarView.swift
@@ -38,8 +38,13 @@ struct CalendarView: View {
                 }
             }.padding([.leading, .trailing], 36)
             HStack{
-                
-            }
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 30), spacing: 18)],
+                    spacing: 10) {
+                    ForEach(0..<31){ i in
+                    DayView(letter: "i", firstCheck: true, secondCheck: true, thirdCheck: true)
+                    }
+                }
+            }.padding([.leading, .trailing], 36)
         }
     }
 }
@@ -59,5 +64,43 @@ struct DayOfWeekView: View {
                 .font(.custom("SpoqaHanSansNeo-Medium",size: 13))
                 .foregroundColor(Color("plicBlack"))
         }
+    }
+}
+
+struct DayView: View {
+    let letter: String
+    @State var firstCheck: Bool
+    @State var secondCheck: Bool
+    @State var thirdCheck: Bool
+    
+    var body: some View{
+        VStack{
+            HStack{
+                Text("\(letter)")
+                    .font(.custom("SpoqaHanSansNeo-Bold",size: 13))
+                    .foregroundColor(Color("plicBlack"))
+            }
+            HStack{
+                Spacer()
+                if(firstCheck){
+                    Circle()
+                        .fill(Color("plicCirclepink"))
+                        .frame(width: 6, height: 6)
+                    Spacer()
+                }
+                if(secondCheck){
+                    Circle()
+                        .fill(Color("plicCircleyellow"))
+                        .frame(width: 6, height: 6)
+                    Spacer()
+                }
+                if(thirdCheck){
+                    Circle()
+                        .fill(Color("plicCirclesky"))
+                        .frame(width: 6, height: 6)
+                    Spacer()
+                }
+            }
+        }.frame(width: 30, height: 40)
     }
 }
