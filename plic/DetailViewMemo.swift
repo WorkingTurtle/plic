bfefd3db03e0c06954bb342312e140de1f7faf12
Youngseo Yoon <58525719+yys88699@users.noreply.github.com>
2022-06-11 13:25:09 +0900

    💄 세부 일정 메모 생성
    - 메모 UI Layout 제작
    - 입력된 메모 없을 시 기본 입력 텍스트 회색 처리

---
+++ plic/DetailViewMemo.swift
@@ -0,0 +1,61 @@
+//
+//  DetailViewMemo.swift
+//  plic
+//
+//  Created by Youngseo Yoon on 2022/06/11.
+//
+
+import SwiftUI
+
+
+// 작성된 메모 확인란
+struct DetailViewMemo: View {
+    @State var memoText = "비어있는 메모"
+        
+    var body: some View {
+        VStack{
+            HStack{
+                Image(systemName: "square.and.pencil")
+                    .frame(width: 16, height: 18)
+                    .foregroundColor(Color("plicPink"))
+                    .padding(.leading, 24)
+                Text("메모")
+                    .font(.system(size: 14))
+                    .padding(.leading, 5)
+                Spacer()
+            }
+            
+            ZStack(alignment: .topLeading){
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: 350, height: 146)
+                    // plicPurple -> #F5F5F5 추가되면 바뀔 색상
+                    .foregroundColor(Color("plicPurple"))
+                Text("\(memoText)")
+                    .font(.system(size: 14))
+                    .foregroundColor(changeMemoTextColor(text: memoText))
+                    .padding(12)
+            }
+        }
+    }
+}
+
+struct DetailViewMemo_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailViewMemo()
+    }
+}
+
+
+
+// 메모하지 않았을 경우 텍스트를 회색으로 처리, 했을 경우 검정으로 처리
+func changeMemoTextColor(text: String) -> Color {
+    var textColor: Color
+    
+    if text == "비어있는 메모" {
+        textColor = .gray
+        return textColor
+    } else {
+        textColor = .black
+        return textColor
+    }
+}
