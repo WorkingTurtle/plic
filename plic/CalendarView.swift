06163584acf85ac5a29813990eacd320468444c8
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-06-11 17:31:30 +0900

    💄 Add CalendarView

---
+++ plic/CalendarView.swift
@@ -0,0 +1,20 @@
+//
+//  CalendarView.swift
+//  plic
+//
+//  Created by 이주화 on 2022/06/11.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarView()
+    }
+}
