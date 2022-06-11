157997c9ad7c81381d87384c9eb56bed58a228df
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-06-12 00:28:53 +0900

    :lipstick: Update CalendarView

--- plic/CalendarView.swift
+++ plic/CalendarView.swift
@@ -10,41 +10,54 @@ import SwiftUI
 struct CalendarView: View {
     @State var year: String = "2022"
     @State var month: String = "6"
-    let dayoftheWeek : [String] = ["SUM", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+    let dayoftheWeek : [String] = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+    let columns = [
+          //추가 하면 할수록 화면에 보여지는 개수가 변함
+            GridItem(.flexible()),
+            GridItem(.flexible()),
+            GridItem(.flexible()),
+            GridItem(.flexible()),
+            GridItem(.flexible()),
+            GridItem(.flexible()),
+            GridItem(.flexible())
+        ]
     
     var body: some View {
         VStack{
             HStack{
                 VStack{
-                    Text("\(year)년 \(month)월 >")
-                        .font(.custom("SpoqaHanSansNeo-Medium",size: 22))
+                    Text("\(year)년 \(month)월")
+                        .font(.custom("SpoqaHanSansNeo-Bold",size: 16))
+                        .foregroundColor(Color("plicPink"))
+                }
+                VStack{
+                    Image(systemName: "chevron.right")
+                        .font(.custom("SpoqaHanSansNeo-Bold",size: 16))
                         .foregroundColor(Color("plicPink"))
                 }
                 Spacer()
                 VStack{
                     Image(systemName: "plus")
-                        .font(.custom("SpoqaHanSansNeo-Medium",size: 22))
+                        .font(.custom("SpoqaHanSansNeo-Medium",size: 16))
                         .foregroundColor(Color("plicPink"))
                 }
             }
-            .padding([.leading, .trailing], 36)
+            .padding([.leading, .trailing], 32)
             .padding(.bottom, 15)
             HStack{
-                ForEach(0..<7){ i in
+                LazyVGrid(columns: columns) {
+                    ForEach(0..<7){ i in
                     DayOfWeekView(letter: dayoftheWeek[i])
-                    if(i < 6){
-                        Spacer()
-                    }
                 }
-            }.padding([.leading, .trailing], 36)
+                }
+            }.padding([.leading, .trailing], 32)
             HStack{
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 30), spacing: 18)],
-                    spacing: 10) {
-                    ForEach(1..<32){ i in
+                LazyVGrid(columns: columns) {
+                    ForEach(1..<42){ i in
                     DayView(letter: i, firstCheck: true, secondCheck: true, thirdCheck: true)
                     }
                 }
-            }.padding([.leading, .trailing], 36)
+            }.padding([.leading, .trailing], 32)
         }
     }
 }
@@ -61,7 +74,7 @@ struct DayOfWeekView: View {
     var body: some View {
         VStack{
             Text("\(letter)")
-                .font(.custom("SpoqaHanSansNeo-Medium",size: 13))
+                .font(.custom("SpoqaHanSansNeo-Bold",size: 13))
                 .foregroundColor(Color("plicBlack"))
         }
     }
@@ -74,31 +87,28 @@ struct DayView: View {
     @State var thirdCheck: Bool
     
     var body: some View{
-        VStack{
+        VStack(spacing: 2){
             HStack{
                 Text("\(letter)")
                     .font(.custom("SpoqaHanSansNeo-Bold",size: 13))
                     .foregroundColor(Color("plicBlack"))
-            }
-            HStack{
-                Spacer()
+            }.frame(width: 32, height: 32)
+            HStack(spacing: 2){
                 if(firstCheck){
                     Circle()
                         .fill(Color("plicCirclepink"))
                         .frame(width: 6, height: 6)
-                    Spacer()
                 }
                 if(secondCheck){
                     Circle()
                         .fill(Color("plicCircleyellow"))
                         .frame(width: 6, height: 6)
-                    Spacer()
                 }
                 if(thirdCheck){
                     Circle()
                         .fill(Color("plicCirclesky"))
                         .frame(width: 6, height: 6)
-                    Spacer()
+
                 }
             }
         }.frame(width: 30, height: 40)
