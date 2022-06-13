45a76a006a5d9eb16d7caad81ea57d59a9f6cf7c
최홍준 <102945279+Hongjaaa@users.noreply.github.com>
2022-06-13 21:12:59 +0900

    :twisted_rightwards_arrows: 프로실 설정 창 (#37)
    설정 창 구성 중
    - 본인, 상대 애칭 설정
    - 사귀게 된 날 설정
    
    Co-authored-by: Juhwa Lee <juhwa.lee1023@gmail.com>

---
+++ plic/SettingViews/SettingProfileView.swift
@@ -0,0 +1,87 @@
+//
+//  SettingProfileView.swift
+//  plic
+//
+//  Created by 최홍준 on 2022/06/13.
+//
+
+import SwiftUI
+
+struct SettingProfileView: View {
+    
+    // 뒤로 나가기
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    // 본인의 애칭 수정
+    @State var myNickName: String = ""
+    // 상대방 애칭 수정
+    @State var opponentNickName: String = ""
+    // 처음 사귀게 된 날짜 수정
+    @State var firstDay = Date()
+    
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .leading, spacing: 15) {
+                Text("애칭 수정")
+                    .font(.custom("SpoqaHanSansNeo-Bold", size: 17))
+                    .padding(EdgeInsets(top: 65, leading: 20, bottom: 10, trailing: 0))
+                TextField("본인의 애칭을 입력해 주세요", text: $myNickName)
+                    .font(Font.custom("SpoqaHanSansNeo-Regular", size: 17))
+                    .disableAutocorrection(true)
+                // 텍스트 입력 시 지우기 (X) 버튼 나타나기
+                    .modifier(TextFieldClearButton(scheduleNameText: $myNickName))
+                    .multilineTextAlignment(.leading)
+                    .padding(EdgeInsets(top: 11, leading: 17, bottom: 11, trailing: 15))
+                    .background(RoundedRectangle(cornerRadius: 10)
+                        .foregroundColor(Color("plicLightestgrey")))
+                    .padding(.horizontal, 20)
+                TextField("상대방의 애칭을 입력해 주세요", text: $opponentNickName)
+                    .font(Font.custom("SpoqaHanSansNeo-Regular", size: 17))
+                    .disableAutocorrection(true)
+                // 텍스트 입력 시 지우기 (X) 버튼 나타나기
+                    .modifier(TextFieldClearButton(scheduleNameText: $opponentNickName))
+                    .multilineTextAlignment(.leading)
+                    .padding(EdgeInsets(top: 11, leading: 17, bottom: 11, trailing: 15))
+                    .background(RoundedRectangle(cornerRadius: 10)
+                        .foregroundColor(Color("plicLightestgrey")))
+                    .padding(.horizontal, 20)
+                
+                Text("사귀게 된 날짜 수정")
+                    .font(.custom("SpoqaHanSansNeo-Bold", size: 17))
+                    .padding(EdgeInsets(top: 65, leading: 20, bottom: 10, trailing: 0))
+                DatePicker("", selection: $firstDay, displayedComponents: .date)
+                    .font(.custom("SpoqaHanSansNeo-Regular", size: 17))
+                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 10, trailing: 20))
+//                Text("\(firstDay, formatter: dateFormatter)")
+                
+                Spacer()
+                
+                Button(action: {
+                    /* 내용 저장하고 이전 페이지로 이동 */
+                }) {
+                    Text("완료")
+                        .font(.custom("SpoqaHanSansNeo-Bold", size: 18))
+                        .foregroundColor(Color("plicPink"))
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .padding(.vertical, 18)
+                        .background(RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color("plicPeach")).cornerRadius(10))
+                        .padding(.horizontal, 20)
+                }
+                .padding(.bottom, 31)
+                
+                .navigationBarTitle("프로필 수정", displayMode: .inline).font(.custom("SpoqaHanSansNeo-Bold", size: 17))
+                .navigationBarItems(leading:
+                                        Button(action: {self.presentationMode.wrappedValue.dismiss()}) {
+                    Image(systemName: "chevron.left")
+                }
+                    .font(.custom("SpoqaHanSansNeo-Regular", size: 17)).foregroundColor(ColorManage.plicPink))
+            }
+        }
+    }
+}
+
+struct SettingProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingProfileView()
+    }
+}
