dc432288a95afdb324a6ff548c8aea2ab785aed9
2youngjun <86882798+2youngjun@users.noreply.github.com>
2022-06-13 16:40:00 +0900

    :twisted_rightwards_arrows: :zap: Check iCloudStatus & Alert (#29)
    - Check iCloudStatus & Alert

--- plic/OnboardingUsernameView.swift
+++ plic/OnboardingUsernameView.swift
@@ -8,18 +8,13 @@
 import SwiftUI
 
 struct OnboardingUsernameView: View {
-    @Binding var show: Bool
     var body: some View {
-        Button(action: {
-            self.show.toggle()
-        }){
-            Text("Move")
-        }
+        Text("Hello World")
     }
 }
 
 struct OnboardingUsernameView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardingUsernameView(show: .constant(true))
+        OnboardingUsernameView()
     }
 }
