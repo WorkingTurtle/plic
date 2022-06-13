8819fb54a7cf546021ec3585fbbcf072cecdbc19
Youngseo Yoon <58525719+yys88699@users.noreply.github.com>
2022-06-13 15:40:10 +0900

    💄 온보딩 애칭 입력 UI Layout 생성

--- plic/plicApp.swift
+++ plic/plicApp.swift
@@ -13,8 +13,9 @@ struct plicApp: App {
     
     var body: some Scene {
         WindowGroup {
-            TapBarMainView(tabbarIndex: 0)
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            OnboardingUsernameView()
+//            TapBarMainView(tabbarIndex: 0)
+//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
