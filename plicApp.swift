e788579431378aace000ba015c249b4570774815
Youngseo Yoon <58525719+yys88699@users.noreply.github.com>
2022-06-14 17:41:58 +0900

    ✏️ 빌드 화면 원위치

--- plicApp.swift
+++ plicApp.swift
@@ -12,12 +12,9 @@ struct plicApp: App {
     let persistenceController = PersistenceController.shared
     
     var body: some Scene {
-        
         WindowGroup {
-//            OnboardingDateOfBirth()
-            OnboardingUsernameView()
-//            TapBarMainView(tabbarIndex: 0)
-//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            TapBarMainView(tabbarIndex: 0)
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
