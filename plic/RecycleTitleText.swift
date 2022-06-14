80c6db51e70320d11ed12b998ffcadd2d38136e4
yys88699 <58525719+yys88699@users.noreply.github.com>
2022-06-14 22:00:46 +0900

    :twisted_rightwards_arrows: Feat/onboarding/link navigation (#43)
    * ⚡️ 불필요한 성능저하 코드 삭제
    
    - 코선생 존경합니다
    
    * 🎨 온보딩 뷰 연결
    
    - RecycleNextButton 파일 삭제
    - 뷰 엮기
    -  layout 패딩 간격 재조정
    
    * ✏️ 코드 정렬
    
    * ✏️ 빌드 화면 원위치
    
    * 🔥 Bundle identifer 원위치

--- plic/RecycleTitleText.swift
+++ plic/RecycleTitleText.swift
@@ -8,17 +8,21 @@
 import SwiftUI
 
 struct RecycleTitleText: View {
-    @Binding var titleText: String
-    @Binding var subtitleText: String
+    var titleText: String
+    var subtitleText: String
     var body: some View {
-        VStack(alignment: .leading, spacing: 15){
+        VStack(alignment: .leading, spacing: 0){
             Text("\(titleText)")
                 .font(Font.custom("SpoqaHanSansNeo-Bold", size: 25))
-                .padding(EdgeInsets(top: 308, leading: 20, bottom: 0, trailing: 0))
+                .lineLimit(nil)
+                .multilineTextAlignment(.leading)
+                .padding(.leading, 20)
+                .padding(.bottom, 15)
             Text("\(subtitleText)")
                 .font(Font.custom("SpoqaHanSansNeo-Regular", size: 13))
                 .foregroundColor(Color("plicGrey"))
                 .padding(.leading, 20)
+                .padding(.bottom, 30)
         }
     }
 }
