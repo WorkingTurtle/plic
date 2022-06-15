658634d8b72364510cbdb946383d6fb4d6aebb76
박원빈 <wonbin9413@postech.ac.kr>
2022-06-15 17:35:05 +0900

    🚧 CloudKit 작업 중...

---
+++ SceneDelegate.swift
@@ -0,0 +1,72 @@
+//
+//  SceneDelegate.swift
+//  plic
+//
+//  Created by 박원빈 on 2022/06/15.
+//
+
+import UIKit
+import SwiftUI
+import CloudKit
+
+class SceneDelegate: NSObject, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+      
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+      
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+      
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+      
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+      
+    }
+    
+    func windowScene(_ windowScene: UIWindowScene, userDidAcceptCloudKitShareWith cloudKitShareMetadata: CKShare.Metadata) {
+        guard cloudKitShareMetadata.containerIdentifier == CKConstant.containerIdentifier else {
+            print("Shared container identifier \(cloudKitShareMetadata.containerIdentifier) did not match known identifier.")
+            return
+        }
+
+        // Create an operation to accept the share, running in the app's CKContainer.
+        let container = CKContainer(identifier: CKConstant.containerIdentifier)
+        let operation = CKAcceptSharesOperation(shareMetadatas: [cloudKitShareMetadata])
+
+        debugPrint("Accepting CloudKit Share with metadata: \(cloudKitShareMetadata)")
+
+        operation.perShareResultBlock = { metadata, result in
+            let rootRecordID = metadata.rootRecordID
+
+            switch result {
+            case .failure(let error):
+                debugPrint("Error accepting share with root record ID: \(rootRecordID), \(error)")
+
+            case .success:
+                debugPrint("Accepted CloudKit share for root record ID: \(rootRecordID)")
+            }
+        }
+
+        operation.acceptSharesResultBlock = { result in
+            if case .failure(let error) = result {
+                debugPrint("Error accepting CloudKit Share: \(error)")
+            }
+        }
+
+        operation.qualityOfService = .utility
+        container.add(operation)
+    }
+}
---
+++ plic/Cloudkit/Model/Couple.swift
@@ -0,0 +1,61 @@
+//
+//  Couple.swift
+//  plic
+//
+//  Created by 박원빈 on 2022/06/15.
+//
+
+import Foundation
+import CloudKit
+
+struct Couple {
+    let uuid: String
+    let createdAt: Date
+    let updatedAt: Date
+    
+    var nicknameOwner: String?
+    var nicknamePartner: String?
+    var firstMeetingDate: Date?
+    var ownerBirthDay: Date?
+    var partnerBirthDay: Date?
+}
+
+extension Couple: Record {
+    
+    init() {
+        self.uuid = UUID().uuidString
+        self.createdAt = Date()
+        self.updatedAt = Date()
+    }
+    
+    init(record: CKRecord) {
+        self.uuid = record.recordID.recordName
+        self.createdAt = record.creationDate!
+        self.updatedAt = record.modificationDate!
+        self.nicknameOwner = record.object(forKey: CKConstant.Field.nicknameOwner) as! String?
+        self.nicknamePartner = record.object(forKey: CKConstant.Field.nicknamePartner) as! String?
+        self.firstMeetingDate = record.object(forKey: CKConstant.Field.firstMeetingDate) as! Date?
+        self.ownerBirthDay = record.object(forKey: CKConstant.Field.ownerBirthDay) as! Date?
+        self.partnerBirthDay = record.object(forKey: CKConstant.Field.partnerBirthDay) as! Date?
+    }
+    
+    func getRecordID() -> CKRecord.ID {
+        let zoneID = CloudKitManager.coupleZone.zoneID
+        let recordID = CKRecord.ID(recordName: uuid, zoneID: zoneID)
+        return recordID
+    }
+    
+    func convertToCKRecord() -> CKRecord {
+        let record = CKRecord(recordType: CKConstant.RecordType.Couple, recordID: getRecordID())
+        return mergeWithCKRecord(record)
+    }
+    
+    func mergeWithCKRecord(_ record: CKRecord) -> CKRecord {
+        record.setValue(nicknameOwner, forKey: CKConstant.Field.nicknameOwner)
+        record.setValue(nicknamePartner, forKey: CKConstant.Field.nicknamePartner)
+        record.setValue(firstMeetingDate, forKey: CKConstant.Field.firstMeetingDate)
+        record.setValue(ownerBirthDay, forKey: CKConstant.Field.ownerBirthDay)
+        record.setValue(partnerBirthDay, forKey: CKConstant.Field.partnerBirthDay)
+        return record
+    }
+}
