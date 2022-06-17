6706a18cf3bc07fb76c1584331340685d69b37d8
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-06-17 17:47:23 +0900

    :sparkles: Add allDayTogle

--- plic/MainView/MainAddSchedule.swift
+++ plic/MainView/MainAddSchedule.swift
@@ -41,14 +41,26 @@ struct MainAddSchedule: View {
                     Toggle("하루 종일", isOn: $allDayToggle)
                         .font(.custom("SpoqaHanSansNeo-Regular", size: 17))
                         .toggleStyle(SwitchToggleStyle(tint: Color("plicPink")))
-                    
-                    DatePicker("시작", selection: $timeStart, in: Date()...)
-                        .font(.custom("SpoqaHanSansNeo-Regular", size: 17))
-                        .accentColor(Color("plicPink"))
-                    DatePicker("종료", selection: $timeEnd, in: timeStart...)
-                        .font(.custom("SpoqaHanSansNeo-Regular", size: 17))
-                        .accentColor(Color("plicPink"))
+                    // toggle을 눌렀을 때, 날짜만 설정할 수 있도록 if문
+                    if allDayToggle {
+                        // 전 날을 체크할 수 없도록 in: Date 사용
+                        DatePicker("시작", selection: $timeStart, in: Date()..., displayedComponents: .date)
+                            .font(.custom("SpoqaHanSansNeo-Regular", size: 17))
+                            .accentColor(Color("plicPink"))
+                        // 시작 날짜보다 이전의 날짜를 선택하지 못하게끔 in: timeStart... 사용
+                        DatePicker("종료", selection: $timeEnd, in: timeStart..., displayedComponents: .date)
+                            .font(.custom("SpoqaHanSansNeo-Regular", size: 17))
+                            .accentColor(Color("plicPink"))
+                     } else {
+                         DatePicker("시작", selection: $timeStart, in: Date()...)
+                             .font(.custom("SpoqaHanSansNeo-Regular", size: 17))
+                             .accentColor(Color("plicPink"))
+                         DatePicker("종료", selection: $timeEnd, in: timeStart...)
+                             .font(.custom("SpoqaHanSansNeo-Regular", size: 17))
+                             .accentColor(Color("plicPink"))
+                     }
                 }
+                    
                 
                 Section(header: Text("누구의 일정인가요?").padding(.leading, -20)) {
                     Picker(selection: $whoSchedule, label: Text("")){
